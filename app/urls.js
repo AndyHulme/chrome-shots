0922cd75bc59051b841eea73701cfd4546d0647f
AndyHulme <andyhulme@iMac.local>
2018-04-17 09:23:37 +0100

    updated device & url list for preliminary test

--- app/urls.js
+++ app/urls.js
@@ -1,6 +1,19 @@
 let urls = [
-    'http://example.com',
-    'http://test.at/'
+    'https://www.destinology.co.uk/',
+    'https://www.destinology.co.uk/luxury-holidays/',
+    'https://www.destinology.co.uk/information/aboutdestinology/',
+    'https://www.destinology.co.uk/information/wilmslowstore/',
+    'https://www.destinology.co.uk/careers/',
+    'https://www.destinology.co.uk/information/termsandconditions',
+    'https://www.destinology.co.uk/support/contactdestinology/',
+    'https://www.destinology.co.uk/modern-slavery-statement/',
+    'https://www.destinology.co.uk/information/cookiepolicy/',
+    'https://www.destinology.co.uk/information/privacypolicy/',
+    'https://www.destinology.co.uk/information/frequentlyaskedquestions/',
+    'https://www.destinology.co.uk/sitemap/',
+    'https://www.destinology.co.uk/information/deliverypolicy/',
+    'https://www.destinology.co.uk/brandcollections/brand/sandals-and-beaches/2/',
+    'https://www.destinology.co.uk/brandcollections/brands/'
 ]
 
 module.exports = urls
